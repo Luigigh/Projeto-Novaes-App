3bdffe34a448794ba2485e26417a963eafd65b17
Luigifbruno <168237799+Luigifbruno@users.noreply.github.com>
2024-05-15 15:41:37 -0300

    Add files via upload

--- src/pages/employee/ClientList/index.js
+++ src/pages/employee/ClientList/index.js
@@ -1,85 +1,70 @@
-import React, { useState } from "react";
-import { View, TouchableOpacity, TextInput, Image, Text } from "react-native";
-import { useRoute } from "@react-navigation/native";
-import Header from "../../../components/Header";
-import Footer from "../../../components/Footer";
-import styles from "./Styles";
-import IconSearch from "react-native-vector-icons/FontAwesome";
-import colors from "../../../color";
-import { usuarios } from "../../../service/UserService";
-import ModalInfoClient from "../../../components/ModalInfoClient";
+import { StyleSheet } from "react-native";
 
-const ClientList = ({ navigation }) => {
-  const route = useRoute();
-  const [searchQuery, setSearchQuery] = useState("");
-  const [modalVisibleEdit, setModalVisibleEdit] = useState(false);
+const styles = StyleSheet.create({
+  main: {
+    width: "100%",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    backgroundColor: "white",
+    width: "100%",
+  },
+  body: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  search: {
+    backgroundColor: "transparent",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  inputsearch: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    width: "90%",
+    padding: 8,
+    marginVertical: 10,
+    fontSize: 20,
+    position: "relative",
+    borderWidth: 1,
+    borderColor: '#083C52'
+  },
+  iconcamera: {
+    position: "absolute",
+    top: -15,
+    right: 15,
+  },
+  btnContato: {
+    width: '90%',
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingBottom: 3,
+    borderBottomWidth: 1,
+    borderBottomColor: '#083C52',
+    marginVertical: 10
+  },
+  contato: {
+    backgroundColor: '#F3F3F3',
+    width: '100%',
+    marginVertical: 2,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    margin: 10,
+  },
+  textName: {
+    fontSize: 20,
+    fontWeight: "400",
+    color: '#083C52',
+  },
+});
 
-  const openModal = () => {
-    setModalVisibleEdit(true);
-  };
-
-  const closeModal = () => {
-    setModalVisibleEdit(false);
-  };
-
-  const handleSubmit = (data) => {
-    console.log("Dados atualizados:", data);
-    closeModal();
-  };
-
-  const filteredContacts = usuarios.filter((user) =>
-    user.username.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
-  const handleSearch = () => {
-    console.log("Pesquisando:", searchQuery);
-  };
-
-  return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.body}>
-        <View style={styles.search}>
-          <TextInput
-            style={styles.inputsearch}
-            placeholder="Pesquisar contatos"
-            onChangeText={(text) => setSearchQuery(text)}
-            value={searchQuery}
-          />
-          <TouchableOpacity onPress={handleSearch}>
-            <IconSearch
-              name="search"
-              size={28}
-              color={colors.primary}
-              style={styles.iconcamera}
-            />
-          </TouchableOpacity>
-        </View>
-
-        {filteredContacts.length === 0 ? (
-          <Text style={styles.notFoundText}>Contato não encontrado</Text>
-        ) : (
-          filteredContacts.map((user, index) => (
-            <TouchableOpacity
-              key={index}
-              style={styles.btnContato}
-              onPress={openModal}
-            >
-                <Text style={styles.textName}>{user.username}</Text>
-            </TouchableOpacity>
-          ))
-        )}
-      </View>
-      <Footer routeSelected={route.name} />
-
-      <ModalInfoClient
-        visible={modalVisibleEdit}
-        onClose={closeModal}
-        onSubmit={handleSubmit}
-        initialData={{ name: "Ola", lastName: "", login: "" }}
-      />
-    </View>
-  );
-};
-
-export default ClientList;
+export default styles;

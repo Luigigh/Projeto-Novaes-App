7af0032a746869031bb68884697e669c99b73671
Luigifbruno <168237799+Luigifbruno@users.noreply.github.com>
2024-05-15 15:40:13 -0300

    Add files via upload

--- src/pages/employee/UsersList/Styles/index.js
+++ src/pages/employee/UsersList/Styles/index.js
@@ -1,36 +1,69 @@
-// Styles.js
 import { StyleSheet } from "react-native";
-import colors from '../../../../color';
 
 const styles = StyleSheet.create({
+  main: {
+    width: "100%",
+  },
   container: {
     flex: 1,
-  },
-  main: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-    paddingHorizontal: 20,
-  },
-  title:{
-    color: 'black',
-    fontSize: 32,
-    fontWeight: "500",
-    marginBottom: 20,
-    marginTop: 15,
-  },
-  itemContainer: {
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: colors.primary,
-    width: "100%",
-    flexDirection: "row",
     justifyContent: "space-between",
+    backgroundColor: "white",
+    width: "100%",
   },
-  itemText: {
-    fontSize: 16,
-    color: colors.primary,
+  body: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  search: {
+    backgroundColor: "transparent",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  inputsearch: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    width: "90%",
+    padding: 8,
+    marginVertical: 10,
+    fontSize: 20,
+    position: "relative",
+    borderWidth: 1,
+    borderColor: '#083C52'
+  },
+  iconcamera: {
+    position: "absolute",
+    top: -15,
+    right: 15,
+  },
+  btnContato: {
+    width: '90%',
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingBottom: 3,
+    borderBottomWidth: 1,
+    borderBottomColor: '#083C52',
+    marginVertical: 10
+  },
+  contato: {
+    backgroundColor: '#F3F3F3',
+    width: '100%',
+    marginVertical: 2,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    margin: 10,
+  },
+  textName: {
+    fontSize: 20,
+    fontWeight: "400",
+    color: '#083C52',
   },
 });
 
--- src/pages/employee/UsersList/index.js
+++ src/pages/employee/UsersList/index.js
@@ -1,57 +1,101 @@
-// UsersList.js
 import React, { useState, useEffect } from "react";
-import { View, Image, Text, TouchableOpacity, ScrollView, FlatList } from "react-native";
+import { View, TouchableOpacity, TextInput, Text } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import Header from "../../../components/Header";
 import Footer from "../../../components/Footer";
-import { useUser } from "../../../context/index.js";
-import { getAllUsers } from "../../../service/UserService";
 import styles from "./Styles";
-import colors from '../../../color';
-import ModalInfoClient from '../../../components/ModalInfoClient';
+import IconSearch from "react-native-vector-icons/FontAwesome";
+import colors from "../../../color";
+import ModalInfoClient from "../../../components/ModalInfoClient";
+import { getAllUsers } from "../../../service/UserService";
 
-const UsersList = ({ navigation }) => {
+const ClientList = ({ navigation }) => {
   const route = useRoute();
-  const [users, setUsers] = useState([]);
-  const { user } = useUser();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [clients, setClients] = useState([]);
+  const [modalVisibleEdit, setModalVisibleEdit] = useState(false);
 
   useEffect(() => {
-    getUsers();
+    fetchUsers();
   }, []);
 
-  const getUsers = async () => {
+  const fetchUsers = async () => {
     try {
       const usersList = await getAllUsers();
-      setUsers(usersList);
+      setClients(usersList);
     } catch (error) {
       console.error("Erro ao obter lista de clientes:", error);
     }
   };
 
-  const renderItem = ({ item }) => (
-    <TouchableOpacity style={styles.itemContainer} onPress={() => navigation.navigate("InfoManager")}>
-      <Text style={styles.itemText}>{item.name} {item.lastname}</Text>
-      {/* <Text style={styles.itemText}>{item.email}</Text>
-      <Text style={styles.itemText}>{item.enterprise_name}</Text> */}
-    </TouchableOpacity>
+  const openModal = () => {
+    setModalVisibleEdit(true);
+  };
+
+  const closeModal = () => {
+    setModalVisibleEdit(false);
+  };
+
+  const handleSubmit = (data) => {
+    console.log("Dados atualizados:", data);
+    closeModal();
+  };
+
+  const filteredClients = clients.filter((client) =>
+    client.username.toLowerCase().includes(searchQuery.toLowerCase())
   );
+
+  const handleSearch = () => {
+    console.log("Pesquisando:", searchQuery);
+  };
 
   return (
     <View style={styles.container}>
       <Header />
-      <View style={styles.main}>
-        <Text style={styles.title}>Lista de clientes</Text>
-        <FlatList
-          data={users}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id.toString()}
-          style={{ width: "100%" }}
-        />
+      <View style={styles.body}>
+        <View style={styles.search}>
+          <TextInput
+            style={styles.inputsearch}
+            placeholder="Pesquisar contatos"
+            onChangeText={(text) => setSearchQuery(text)}
+            value={searchQuery}
+          />
+          <TouchableOpacity onPress={handleSearch}>
+            <IconSearch
+              name="search"
+              size={28}
+              color={colors.primary}
+              style={styles.iconcamera}
+            />
+          </TouchableOpacity>
+        </View>
+
+        {filteredClients.length === 0 ? (
+          <Text style={styles.notFoundText}>Contato não encontrado</Text>
+        ) : (
+          filteredClients.map((client, index) => (
+            <TouchableOpacity
+              key={index}
+              style={styles.btnContato}
+              onPress={openModal}
+            >
+              <Text
+                style={styles.textName}
+              >{`${client.name} ${client.lastname}`}</Text>
+            </TouchableOpacity>
+          ))
+        )}
       </View>
-      <Footer style={styles.footer} routeSelected={route.name} />
+      <Footer routeSelected={route.name} />
+
+      <ModalInfoClient
+        visible={modalVisibleEdit}
+        onClose={closeModal}
+        onSubmit={handleSubmit}
+        initialData={{ name: "Ola", lastName: "", login: "" }}
+      />
     </View>
   );
 };
 
-export default UsersList;
-
+export default ClientList;
